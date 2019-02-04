c0c4ac5ff5b902b46e435f411c73b6bd6961195b
Blueheim <crystalrod0@gmail.com>
2019-02-04 15:48:51 +0100

    progress

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const httpObservable = (url, urlParams = {}, urlBody = {}) => {
 
     const urlInstance = new URL(url);
 
+    console.log(URLSearchParams);
     urlInstance.search = new URLSearchParams(urlParams);
 
     fetch(url, { ...urlBody, signal })
