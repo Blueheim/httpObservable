ef56a20a49100a47fb1e75e6527400b67e3a73f9
Blueheim <crystalrod0@gmail.com>
2019-02-04 14:28:17 +0100

    add polyfills and searchparams

--- index.js
+++ index.js
@@ -1,28 +1,34 @@
-const { Observable } =  require('rxjs/Observable');
+const { Observable } = require('rxjs/Observable');
+const URLSearchParams = require('@ungap/url-search-params');
+require('whatwg-fetch');
 
-const httpObservable = (url, urlBody) => {
+const httpObservable = (url, urlParams = {}, urlBody = {}) => {
   return Observable.create(observer => {
     const controller = new AbortController();
     const signal = controller.signal;
 
-    window.fetch(url, { ...urlBody, signal})
-    .then(response => {
-      if (response.ok) {
-        return response.json();
-      } else {
-        observer.error('Request failed with status code:' + response.status);
-      }
-    })
-    .then(body => {
-      observer.next(body);
-      observer.complete();
-    })
-    .catch(err => {
-      observer.error(err);
-    });
+    const urlInstance = new URL(url);
+
+    urlInstance.search = new URLSearchParams(urlParams);
+
+    fetch(url, { ...urlBody, signal })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          observer.error('Request failed with status code:' + response.status);
+        }
+      })
+      .then(body => {
+        observer.next(body);
+        observer.complete();
+      })
+      .catch(err => {
+        observer.error(err);
+      });
 
     return () => controller.abort();
   });
-}
+};
 
 module.exports = httpObservable;
