0e0b07d279d380354a5cdde16f85a330c10c2a50
Blueheim <crystalrod0@gmail.com>
2019-02-04 15:44:05 +0100

    progress

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const { Observable } = require('rxjs/Observable');
 const URLSearchParams = require('@ungap/url-search-params');
-require('whatwg-fetch');
+//require('whatwg-fetch');
 
 const httpObservable = (url, urlParams = {}, urlBody = {}) => {
   return Observable.create(observer => {
